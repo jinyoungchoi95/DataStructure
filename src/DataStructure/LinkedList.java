9c3b7191a4a8a4955fc2f72acca49778bb00dcda
jinyoungchoi <jinyoungchoi95@gmail.com>
2021-04-20 13:40:16 +0900

    feat: Queue 인터페이스 추가로 인한 메소드 추가

--- src/DataStructure/LinkedList.java
+++ src/DataStructure/LinkedList.java
@@ -4,7 +4,7 @@ import InterFace.List;
 
 import java.util.NoSuchElementException;
 
-public class LinkedList<E> implements List<E> {
+public class LinkedList<E> implements List<E>, Deque<E> {
 
     transient Node<E> first;
     transient Node<E> last;
@@ -386,6 +386,33 @@ public class LinkedList<E> implements List<E> {
             }
         }
         return -1;
+    }
+
+    /**
+     * 큐의 가장 처음 위치에 특정 요소를 추가합니다.
+     * @param e 큐의 처음 위치에 추가할 특정 요소.
+     * @return 큐에 특정 요소가 추가된 경우 {@code true}, 추가되지 않은 경우 {@code false}를 반환
+     */
+    public boolean offer(E e) {
+        return add(e);
+    }
+
+    /**
+     * 큐의 가장 처음 요소를 반환합니다.
+     * @return 큐의 최상단 요소
+     */
+    public E peek() {
+        Node<E> node = first;
+        return node == null ? null : node.item;
+    }
+
+    /**
+     * 큐의 가장 처음 요소를 제거하고 반환합니다.
+     * @return 제거된 큐의 요소
+     */
+    public E poll() {
+        Node<E> node = first;
+        return node == null ? null : unlinkFirst();
     }
 
     /**
